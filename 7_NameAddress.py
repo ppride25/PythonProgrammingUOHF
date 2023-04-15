5e68f16aac5a2376d4274f91ec659c56d7fd6e46
John Doe <johndoe@email.com>
2023-04-16 02:56:05 +0800

    fstring

---
+++ 7_NameAddress.py
@@ -0,0 +1,8 @@
+# Write your solution here
+Name=input("Given name:")
+Fam_name=input("Family name: ")
+Street_Add=input("Street address:")
+City=input("City and postal code:")
+print(Name + " "+Fam_name)
+print(Street_Add)
+print(City)
---
+++ 8_Utterances.py
@@ -0,0 +1,5 @@
+# Fix the code
+part1 = input("The 1st part: ")
+part2 = input("The 1st part: ")
+part3 = input("The 1st part: ")
+print(part1 +"-"+ part2 +"-"+ part3+"!")
