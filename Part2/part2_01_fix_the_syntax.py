7f1c636aced0bc0a8d665600fafc71e6205f0ec4
John Doe <johndoe@email.com>
2023-04-24 20:14:08 +0800

    fix the syntax

---
+++ Part2/part2_01_fix_the_syntax.py
@@ -0,0 +1,10 @@
+# Fix the program
+number = int(input("Please type in a number: "))
+if number>100:
+    print("The number was greater than one hundred")
+    number=number-100
+    #print(f"{number}")
+    print("Now its value has decreased by one hundred")
+    print(f"Its value is now {number}")
+print(f" {number} must be my lucky number!")
+print("Have a nice day!")
