31c41598fb4a52d59a819912e06b1760ac956ecb
John Doe <johndoe@email.com>
2023-04-24 20:06:40 +0800

    24

---
+++ float.py
@@ -0,0 +1,6 @@
+# Write your solution here
+
+number=float(input("Please type in a number: "))
+print("Interger part", + int(number))
+number=number-number
+print("Interger part", + float(number))
---
+++ ifsample.py
@@ -0,0 +1,8 @@
+name=input("Enter Your name")
+sample = int(input("Enter Your age: "))
+
+
+if sample >= 18:
+    print((f"congrats  {name} Please come in"))
+else:
+    print("Sorry too young")
